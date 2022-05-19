e77dac06143036831980ae87e90bf23df63a272f
Nathan Hyland <nathan.hyland@atomicfi.com>
2022-05-19 08:31:58 -0500

    sweet todo comments

--- neovimlua/.config/nvim/lua/plugins/configs/luasnip.lua
+++ neovimlua/.config/nvim/lua/plugins/configs/luasnip.lua
@@ -7,36 +7,8 @@ ls.config.set_config({
     -- ext_prio_increase = 1,
     enable_autosnippets = true,
     history = true,
-    updateevents = "TextChanged,TextChangedI"
+    updateevents = "TextChanged,TextChangedI" -- this seems to not work
 })
-
--- some shorthands...
-local sn = ls.snippet_node
-local i = ls.insert_node
-
--- Returns a snippet_node wrapped around an insert_node whose initial
--- text value is set to the current date in the desired format.
-local date_input = function(args, state, fmt)
-    fmt = fmt or "%Y-%m-%d( "
-    return sn(nil, i(1, os.date(fmt)))
-end
-
--- local ts_locals = require 'nvim-treesitter.locals'
--- local ts_utils = require 'nvim-treesitter.ts_utils'
--- local get_node_text = vim.treesitter.get_node_text
-
--- local function get_module(args, snip, old_state, initial_text)
---     local cursor_node = ts_utils.get_node_at_cursor()
---     local scope = ts_locals.get_scope_tree(cursor_node, 0)
---     local query = vim.treesitter.get_query(" )elixir", "@definition.type")
---     print(vim.inspect("query " .. query))
---     print(vim.inspect("scope " .. scope))
---     -- for id, node in query:iter_captures(function_node, 0) do if handlers[node:type()] then return handlers[node:type()](node, info) end end
--- end
---
--- function _G.GetModule()
---     get_module()
--- end
 
 -- https://github.com/tjdevries/config_manager/blob/e96ce10806cafbd4f3a8ff19729b75342857ce71/xdg_config/nvim/after/plugin/luasnip.lua#L64
 -- this is badass, do this
@@ -47,6 +19,7 @@ local lua_snippets = require("snippets.lua")
 local md_snippets = require("snippets.markdown")
 local ts_snippets = require("snippets.typescript")
 local vw_snippets = require("snippets.vimwiki")
+require('snippets.todo-comments') -- just import, it registers itself
 
 ls.add_snippets("all", all_snippets.snippets)
 ls.add_snippets("all", all_snippets.auto_snippets, { type = "autosnippets" })
@@ -60,3 +33,4 @@ ls.add_snippets("typescript", ts_snippets.snippets)
 ls.add_snippets("typescript", ts_snippets.auto_snippets, { type = "autosnippets" })
 ls.add_snippets("vimwiki", vw_snippets.snippets)
 ls.add_snippets("vimwiki", vw_snippets.snippets, { type = "autosnippets" })
+
--- neovimlua/.config/nvim/lua/snippets/go.lua
+++ neovimlua/.config/nvim/lua/snippets/go.lua
@@ -16,18 +16,7 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
-local l = require('luasnip.extras').lambda
-local r = require('luasnip.extras').rep
-local p = require('luasnip.extras').partial
-local m = require('luasnip.extras').match
-local n = require('luasnip.extras').nonempty
-local dl = require('luasnip.extras').dynamic_lambda
 local fmt = require('luasnip.extras.fmt').fmt
-local fmta = require('luasnip.extras.fmt').fmta
-local types = require('luasnip.util.types')
-local conds = require('luasnip.extras.expand_conditions')
-local rep = require("luasnip.extras").rep
 
 local get_node_text = vim.treesitter.get_node_text
 
@@ -207,6 +196,11 @@ return {
 			      local parts = vim.split(import_name[1][1], ".", true)
 			      return string.lower(parts[#parts]) or ""
 			end, { 1 }), i(1) })),
+		s("append", fmt([[{} = append({}, {})]], { -- "baz": Foo.Bar.Baz
+			f(function(import_name)
+			      local parts = vim.split(import_name[1][1], ".", true)
+			      return parts[#parts] or ""
+			end, { 1 }), i(1), i(2)})),
 	--	s("efi", {
 	   --  		i(1, { "val" }),
 	   --  		", ",
