50d48a4d6e728e4b3bde978fb1e1f333f663d9eb
Nathan Hyland <nathan.hyland@gmail.com>
2022-12-13 10:26:57 -0700

    Some cool html fragment highlighting

---
+++ neovimlua/.config/nvim/queries/javascript/injections.scm
@@ -0,0 +1,17 @@
+(call_expression 
+	function: (member_expression 
+			object: (identifier) 
+			property: (property_identifier) @_prop) 
+	arguments: (arguments 
+			(string (string_fragment) @css)) 
+	(#eq? @_prop "locator")
+)
+(
+ [
+	(string_fragment) @html
+  (template_string) @html
+ ]
+ (#contains? @html "<html" "<div>" "<p>" "<body>" "<head>")
+)
+
+
