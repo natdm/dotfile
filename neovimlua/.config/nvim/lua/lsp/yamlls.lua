cc1f63bcf11a7e037d5fcd4e392eaf0d27555768
Nathan Hyland <nathan.hyland@gmail.com>
2022-12-21 07:13:27 -0700

    Reorganize k8s lsp

--- neovimlua/.config/nvim/lua/lsp/yamlls.lua
+++ neovimlua/.config/nvim/lua/lsp/yamlls.lua
@@ -1,47 +1,63 @@
 -- since all things are yaml now adays, use this as a list of correct types for kubernetes to attach the buffer on
 local acceptableVersions = {
-	{ ["kind"] = "CertificateSigningRequest", ["apiVersion"] = "certificates.k8s.io/v1beta1" },
-	{ ["kind"] = "ClusterRoleBinding", ["apiVersion"] = "rbac.authorization.k8s.io/v1" },
-	{ ["kind"] = "ClusterRole", ["apiVersion"] = "rbac.authorization.k8s.io/v1" },
-	{ ["kind"] = "ComponentStatus", ["apiVersion"] = "v1" },
-	{ ["kind"] = "ConfigMap", ["apiVersion"] = "v1" },
-	{ ["kind"] = "ControllerRevision", ["apiVersion"] = "apps/v1" },
-	{ ["kind"] = "CronJob", ["apiVersion"] = "batch/v1beta1" },
-	{ ["kind"] = "DaemonSet", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "Deployment", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "Endpoints", ["apiVersion"] = "v1" },
-	{ ["kind"] = "Event", ["apiVersion"] = "v1" },
-	{ ["kind"] = "HorizontalPodAutoscaler", ["apiVersion"] = "autoscaling/v1" },
-	{ ["kind"] = "Ingress", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "Job", ["apiVersion"] = "batch/v1" },
-	{ ["kind"] = "LimitRange", ["apiVersion"] = "v1" },
-	{ ["kind"] = "Namespace", ["apiVersion"] = "v1" },
-	{ ["kind"] = "NetworkPolicy", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "Node", ["apiVersion"] = "v1" },
-	{ ["kind"] = "PersistentVolumeClaim", ["apiVersion"] = "v1" },
-	{ ["kind"] = "PersistentVolume", ["apiVersion"] = "v1" },
-	{ ["kind"] = "PodDisruptionBudget", ["apiVersion"] = "policy/v1beta1" },
-	{ ["kind"] = "Pod", ["apiVersion"] = "v1" },
-	{ ["kind"] = "PodSecurityPolicy", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "PodTemplate", ["apiVersion"] = "v1" },
-	{ ["kind"] = "ReplicaSet", ["apiVersion"] = "extensions/v1beta1" },
-	{ ["kind"] = "ReplicationController", ["apiVersion"] = "v1" },
-	{ ["kind"] = "ResourceQuota", ["apiVersion"] = "v1" },
-	{ ["kind"] = "RoleBinding", ["apiVersion"] = "rbac.authorization.k8s.io/v1" },
-	{ ["kind"] = "Role", ["apiVersion"] = "rbac.authorization.k8s.io/v1" },
-	{ ["kind"] = "Secret", ["apiVersion"] = "v1" },
-	{ ["kind"] = "ServiceAccount", ["apiVersion"] = "v1" },
-	{ ["kind"] = "Service", ["apiVersion"] = "v1" },
-	{ ["kind"] = "StatefulSet", ["apiVersion"] = "apps/v1" },
+	["certificates.k8s.io/v1beta1"] = { "CertificateSigningRequest" },
+	["rbac.authorization.k8s.io/v1"] = { "ClusterRoleBinding", "ClusterRole", "RoleBinding", "Role" },
+	["v1"] = {
+		"ComponentStatus",
+		"ConfigMap",
+		"Endpoints",
+		"Event",
+		"LimitRange",
+		"Namespace",
+		"Node",
+		"PersistentVolumeClaim",
+		"PersistentVolume",
+		"Pod",
+		"PodTemplate",
+		"ReplicationController",
+		"ResourceQuota",
+		"Secret",
+		"ServiceAccount",
+		"Service",
+	},
+	["apps/v1"] = { "ControllerRevision", "StatefulSet" },
+	["extensions/v1beta1"] = {
+		"DaemonSet",
+		"Deployment",
+		"Ingress",
+		"NetworkPolicy",
+		"PodSecurityPolicy",
+		"ReplicaSet",
+	},
+	["autoscaling/v1"] = { "HorizontalPodAutoscaler" },
+	["batch/v1"] = { "Job" },
+	["batch/v1beta1"] = { "CronJob" },
+	["policy/v1beta1"] = { "PodDisruptionBudget" },
 }
+
+local function split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
 require("lspconfig").yamlls.setup({
 	-- don't allow the client to run if it's not a valid k8s version
+	--
+	-- NOTE: This is a very naiive implementaiton as it requires apiVersion
+	-- to be the first entry and kind to be the second.
 	on_attach = function(client, buffer)
 		local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
-		for _, value in ipairs(acceptableVersions) do
-			local isAPIVersion = lines[1] == "apiVersion: " .. value["apiVersion"]
-			local isKind = lines[2] == "kind: " .. value["kind"]
-			if isAPIVersion and isKind then
+		local k = split(lines[1], ": ")[2]
+		local kinds = acceptableVersions[k]
+		kinds = kinds or {}
+		for _, kind in ipairs(kinds) do
+			if lines[2] == "kind: " .. kind then
 				-- found the right type, don't detach the buffer and let k8s lsp take the wheel.
 				return
 			end
