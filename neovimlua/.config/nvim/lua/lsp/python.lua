c4005d54bad4e2b3b53a6af2acc0925e8c6d7185
Nathan Hyland <nathan.hyland@gmail.com>
2022-12-26 20:03:21 -0700

    py setup

--- neovimlua/.config/nvim/lua/lsp/python.lua
+++ neovimlua/.config/nvim/lua/lsp/python.lua
@@ -1,6 +1,6 @@
 local signature_setup = require("plugins.configs.lspsignature").signature_setup
 
-require("lspconfig").pylsp.setup({
+require("lspconfig").pyright.setup({
 	on_attach = function(client, bufnr)
 		require("lsp_signature").on_attach(signature_setup, bufnr)
 	end,
--- neovimlua/.config/nvim/lua/plugins/configs/snippets/python.lua
+++ neovimlua/.config/nvim/lua/plugins/configs/snippets/python.lua
@@ -2,13 +2,44 @@ local ls = require("luasnip")
 
 -- some shorthands...
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local rep = require("luasnip.extras").rep
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local conds = require("luasnip.extras.expand_conditions")
 
 return {
 	snippets = {
 		s("__main", { t('if __name__ == "__main__":') }),
 	},
-	auto_snippets = {},
+	auto_snippets = {
+		s(
+			"test",
+			d(1, function()
+				return sn(
+					nil,
+					fmt(
+						[[
+								def test_{}(self):
+									{}
+							]],
+						{ i(1, "name"), i(0) }
+					)
+				)
+			end),
+			{
+				condition = function()
+					local path = vim.fn.expand("%:p")
+					local is_py = vim.bo.filetype == "python"
+
+					local line_num = vim.api.nvim__buf_stats(0).current_lnum
+					local content = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)
+					local is_empty = content[1]:match("^%s+")
+					local is_test_file = string.find(path, "/test_") ~= nil
+					return is_test_file and is_py and is_empty
+				end,
+			}
+		),
+	},
 }
