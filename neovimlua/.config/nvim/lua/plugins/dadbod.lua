db8b5c21133edc6f666e17fdd85eb000f90d6833
Nathan Hyland <nathan.hyland@gmail.com>
2024-11-21 06:38:30 -0700

    abbrev/dadbod

---
+++ neovimlua/.config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,9 @@
+  return {
+  -- optional for icon support
+  "tpope/vim-dadbod",
+  dependencies = { 
+    "kristijanhusak/vim-dadbod-ui",
+    "kristijanhusak/vim-dadbod-completion",
+    "tpope/vim-dotenv",
+  }
+}
